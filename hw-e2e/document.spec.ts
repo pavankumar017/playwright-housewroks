1c23d752470dc8037d863bda3882933561bb39d8
Pankaj <jpankaj5555@gmail.com>
2023-11-20 12:30:29 +0530

    adds crud functionality

--- hw-e2e/document.spec.ts
+++ hw-e2e/document.spec.ts
@@ -41,6 +41,7 @@ test.describe('test', async () => {
     })
 
     test("Upload a file", async () => {
+      await page.click('//div[contains(text(),"Patient Master")]/following-sibling::button/span')
       const handle = await page.$(
         'div:nth-child(2) > span > div.ant-upload.ant-upload-select.ant-upload-select-text > span > input[type=file]'
       )
@@ -53,7 +54,7 @@ test.describe('test', async () => {
         '//div[contains(text(),"Press cancel to cancel upload.")]/following-sibling::div/div[2]/div/div/div/div/span/following-sibling::span'
       )
       const expectedFolderNameArray = expectedFolderInnerText.split('\n')
-      expectedFolderName = expectedFolderNameArray[1]
+      expectedFolderName = expectedFolderNameArray[0]
       await page.click('//div/button[2]/span')
       await page.waitForTimeout(2000)
       await page.waitForSelector('//span[@class="text-success"]')
@@ -61,5 +62,32 @@ test.describe('test', async () => {
       
       expect(imageUploaded).toBe('Complete')
     })
+
+    test("Open an image", async () => {
+      await page.click('//button[@aria-label="Close"]')
+      await page.fill('//input[@placeholder="Search"]', '225_daytona-grey_36393e')
+      await page.click(
+        '//*/tbody/tr[2]/td[1]/span[contains(text(),"225_daytona-grey_36393e")]'
+      )
+      await page.waitForTimeout(3000)
+      const imageHeading = await page.innerText('[data-testid="document-manager-title-detail-view"]')
+      expect(imageHeading).toBe(
+        expectedFolderName + '/225_daytona-grey_36393e.jpg'
+      )
+    })
   
   });
+
+  test.describe('Delete file', () => {
+    test('should able to delete the file from detail view', async () => {
+      await page.click('css=[data-testid="document-detail-delete"]')
+      await page.click('//span[contains(text(),"Confirm")]')
+      await page.fill('//input[@placeholder="Search"]', '225_daytona-grey_36393e')
+      await page.waitForTimeout(2000) //to redirect on list view
+      const deletedImageStatus = await page.$(
+        '//*/tbody/tr[2]/td[1]/span[contains(text(),"225_daytona-grey_36393e")]'
+      )
+      // await page.waitForTimeout(2000)
+      expect(deletedImageStatus).toBeNull()
+    })
+  })
