aa0375055ad94c0a339bb97ecdcea3f30b76ae35
RuchetaG <ruchetagogte@gmail.com>
2024-02-06 20:17:10 +0530

    adding more test cases

--- hw-e2e/create-patient.spec.ts
+++ hw-e2e/create-patient.spec.ts
@@ -3,22 +3,104 @@ import { CreatePatient } from "../locators/create-patient";
 import { PatientMaster } from "../locators/patient-master";
 import { SideMenu } from "../locators/side-menu";
 
-test("User should be able to create patient", async ({ page, baseURL }) => {
-  test.setTimeout(100000);
+test("User should be able to open create patient from side menu", async ({
+  page,
+  baseURL,
+}) => {
   await page.goto(`${baseURL}`);
-  const patientMaster = new PatientMaster(page);
-  patientMaster.clickOnCreateButton();
+  const sideMenu = new SideMenu(page);
+  await sideMenu.openCreateFromSideMenu();
   const createPatient = new CreatePatient(page);
-  await createPatient.enterFirstName();
-  await createPatient.enterMiddleName();
-  await createPatient.enterLastName();
-  await createPatient.selectDateOfBirth();
-  await createPatient.clickOnCheckButton();
-  await createPatient.waitTillCreateButtonOnDisplayed();
-  await createPatient.clickOnCreateButton();
+  await createPatient.validateCreatePatientPage();
 });
 
-test.skip("User should be able to clear data from all the fields", async ({
+test("User should be able to open create patient from patient master", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.clickOnCreateButton();
+  const createPatient = new CreatePatient(page);
+  await createPatient.validateCreatePatientPage();
+});
+
+test("Clear button should be enabled in default state", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.clickOnCreateButton();
+  const createPatient = new CreatePatient(page);
+  await createPatient.validateClearButtonEnabled();
+});
+
+// test("Future dates should be disabled in date of birth field", async ({
+//   page,
+//   baseURL,
+// }) => {
+//   await page.goto(`${baseURL}`);
+//   const patientMaster = new PatientMaster(page);
+//   await patientMaster.clickOnCreateButton();
+//   const createPatient = new CreatePatient(page);
+//   await createPatient.validateCreatePatientPage();
+//   await createPatient.validateFutureDateDisabled();
+// });
+
+test("User should get results as per last name entered in search field", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.clickOnCreateButton();
+  const createPatient = new CreatePatient(page);
+  await createPatient.enterLastNameProvided("test");
+  await createPatient.clickOnCheckButton();
+  await createPatient.validateNameSearchResult("test");
+});
+
+test("User should get results as per first name entered in search field", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.clickOnCreateButton();
+  const createPatient = new CreatePatient(page);
+  await createPatient.enterFirstNameProvided("test");
+  await createPatient.clickOnCheckButton();
+  await createPatient.validateNameSearchResult("test");
+});
+
+test("User should get results as per DOB entered in search field", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.clickOnCreateButton();
+  const createPatient = new CreatePatient(page);
+  await createPatient.enterDOBProvided("01/02/2024");
+  await createPatient.clickOnCheckButton();
+  await createPatient.validateDOBSearchResult();
+});
+
+test("No data should be displayed when there are no patients available for selected Sex value", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.clickOnCreateButton();
+  const createPatient = new CreatePatient(page);
+  await createPatient.selectSexValue();
+  await createPatient.clickOnCheckButton();
+  await createPatient.validateNoDataFound();
+});
+
+test("User should be able to clear data from all the fields", async ({
   page,
   baseURL,
 }) => {
@@ -49,22 +131,21 @@ test("User should get error message for mandatory fields", async ({
   await createPatient.validateEmptyMandatoryFieldsErrorMessages();
 });
 
-test.skip("User should be able to use the pagination", async ({
-  page,
-  baseURL,
-}) => {
+test("User should be able to use the pagination", async ({ page, baseURL }) => {
   await page.goto(`${baseURL}`);
   const patientMaster = new PatientMaster(page);
   await patientMaster.clickOnCreateButton();
   const createPatient = new CreatePatient(page);
   const sideMenu = new SideMenu(page);
   await sideMenu.openCreateFromSideMenu();
-  await createPatient.enterDataIntoMandatoryFields();
+  await createPatient.enterFirstNameProvided("test");
+  await createPatient.enterLastNameProvided("test");
+  await createPatient.enterDOBProvided("01/01/2024");
   await createPatient.clickOnCheckButton();
   await createPatient.validatePagination();
 });
 
-test.skip("User should get search result with relevant patients record at top before creating", async ({
+test("User should get search result with relevant patients record at top before creating", async ({
   page,
   baseURL,
 }) => {
@@ -74,7 +155,8 @@ test.skip("User should get search result with relevant patients record at top be
   const createPatient = new CreatePatient(page);
   const sideMenu = new SideMenu(page);
   await sideMenu.openCreateFromSideMenu();
-  await createPatient.enterDataIntoMandatoryFields();
+  await createPatient.enterFirstNameProvided("patient");
+  await createPatient.enterDOBProvided("01/01/2024");
   await createPatient.clickOnCheckButton();
   await createPatient.validateDefaultSort();
 });
@@ -115,13 +197,15 @@ test("User should get error message on entering more than 64 character on last n
   await createPatient.validateErrorOnMaximumCharacter();
 });
 
-test("User should be able to open create patient from side menu", async ({
-  page,
-  baseURL,
-}) => {
+test("User should be able to create patient", async ({ page, baseURL }) => {
   await page.goto(`${baseURL}`);
-  const sideMenu = new SideMenu(page);
-  await sideMenu.openCreateFromSideMenu();
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.clickOnCreateButton();
   const createPatient = new CreatePatient(page);
-  await createPatient.validateCreatePatientPage();
+  await createPatient.enterDataIntoMandatoryFields();
+  await createPatient.enterDataIntoOptionalFields();
+  await createPatient.clickOnCheckButton();
+  await createPatient.waitTillCreateButtonEnabled();
+  await createPatient.clickOnCreateButton();
+  await createPatient.validateSuccessfulCreation();
 });
--- locators/create-patient.ts
+++ locators/create-patient.ts
@@ -1,4 +1,5 @@
 import { Locator, Page, expect } from "@playwright/test";
+import { randomInt } from "crypto";
 import { names, uniqueNamesGenerator } from "unique-names-generator";
 
 export class CreatePatient {
@@ -19,13 +20,20 @@ export class CreatePatient {
   readonly sideMenu: Locator;
   readonly nextPage: Locator;
   readonly patientTable: Locator;
-  randomFirstName: string;
-  randomMiddleName: string;
-  randomLastName: string;
-  randomDateOfBirth: string;
-  firstNameCharacterLimitValue: string;
-  middleNameCharacterLimitValue: string;
-  lastNameCharacterLimitValue: string;
+  readonly randomFirstName: string;
+  readonly randomMiddleName: string;
+  readonly randomLastName: string;
+  readonly randomDateOfBirth: string;
+  readonly firstNameCharacterLimitValue: string;
+  readonly middleNameCharacterLimitValue: string;
+  readonly lastNameCharacterLimitValue: string;
+  readonly noDataAvailable: Locator;
+  readonly sexDropdown: string[];
+  readonly diseaseType: Locator;
+  readonly diseaseTypeDropdown: string[];
+  readonly affectedOrgan: Locator;
+  readonly affectedOrganCancerDropdown: string[];
+  readonly affectedOrganOrganFailureDropdown: string[];
 
   constructor(page: Page) {
     this.page = page;
@@ -33,27 +41,25 @@ export class CreatePatient {
     this.middleName = this.page.getByTestId("middle-name");
     this.lastName = this.page.getByTestId("last-name");
     this.dateOfBirth = this.page.getByTestId("dob");
-    this.sex = this.page.getByTestId("Sex");
+    this.sex = this.page.locator("[data-testid=sex] input");
     this.clearButton = this.page.getByRole("button", { name: "Clear" });
     this.createButton = this.page.getByRole("button", { name: "Create" });
     this.checkButton = this.page.getByTestId("create");
-    this.selectedDate = this.page.locator("[class*=cell-selected]");
+    this.selectedDate = this.page.locator("[class*='selected']");
     this.errorAlert = this.page
       .getByTestId("create-patient-form")
       .getByRole("alert");
     this.totalItems = this.page.getByText("item(s)");
     this.recordsPerPage = this.page.locator(
-      "(//*[@class='ant-table'])/following-sibling::ul[1]/li[@title='Next Page']/following-sibling::li"
+      "(//li[@title='Next Page'])/following-sibling::li[1]"
     );
     this.pageNumberCount = this.page.locator(
-      "(//*[@class='ant-table'])/following-sibling::ul[1]/li[@title='Next Page']/preceding-sibling::li[1]"
+      "(//li[@title='Next Page'])/preceding-sibling::li[1]"
     );
     this.sideMenu = this.page.getByTestId("pin-menu");
-    this.nextPage = this.page.locator(
-      "(//*[@class='ant-table'])/following-sibling::ul[1]/li[@title='Next Page']"
-    );
+    this.nextPage = this.page.locator("(//li[@title='Next Page'])");
     this.patientTable = this.page.locator(
-      "(//*[@class='ant-table'])/div/div/table/tbody"
+      "(//*[@class='ant-table-content'])/table"
     );
     this.randomFirstName = uniqueNamesGenerator({
       dictionaries: [names],
@@ -77,6 +83,39 @@ export class CreatePatient {
       "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
     this.lastNameCharacterLimitValue =
       "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
+    this.noDataAvailable = this.page.getByText(
+      "Looks like we don’t have any patients that match."
+    );
+    this.sexDropdown = ["Male", "Female", "Other", "Choose not to answer"];
+    this.diseaseType = this.page.getByTestId("disease-type");
+    this.diseaseTypeDropdown = ["Cancer", "Organ Failure"];
+    this.affectedOrgan = this.page.locator(
+      "[data-testid='affected-organ'] input"
+    );
+    this.affectedOrganCancerDropdown = [
+      "Breast",
+      "Colon",
+      "Lung",
+      "Liver",
+      "Pancreas",
+      "Gallbladder",
+      "Stomach",
+      "Cervical",
+      "Ovarian",
+      "Uterine",
+      "Prostate",
+      "Thyroid",
+      "Melanoma",
+      "Biliary",
+      "Duodenal]",
+    ];
+    this.affectedOrganOrganFailureDropdown = [
+      "Kidney",
+      "Liver",
+      "Pancreas",
+      "Heart",
+      "Lung",
+    ];
   }
 
   formatDate = (date: Date) => {
@@ -96,6 +135,21 @@ export class CreatePatient {
   async enterLastName() {
     await this.lastName.fill(this.randomLastName);
   }
+
+  async enterLastNameProvided(lastName: string) {
+    await this.lastName.fill(lastName);
+  }
+
+  async enterFirstNameProvided(firstName: string) {
+    await this.firstName.fill(firstName);
+  }
+
+  async enterDOBProvided(DOB: string) {
+    await this.dateOfBirth.click();
+    await this.dateOfBirth.fill(DOB);
+    await this.selectedDate.click();
+  }
+
   async selectDateOfBirth() {
     await this.dateOfBirth.click();
     await this.dateOfBirth.fill(this.randomDateOfBirth);
@@ -109,15 +163,19 @@ export class CreatePatient {
   async clickOnCreateButton() {
     await this.createButton.click();
   }
-  async waitTillCreateButtonOnDisplayed() {
+  async waitTillCreateButtonEnabled() {
     await this.createButton.waitFor();
-    if ((await this.patientTable.count()).toString() == "2") {
+    if ((await this.patientTable.count()).toString() == "1") {
       await expect(
         this.createButton,
         "Create button is not disabled"
       ).toBeDisabled();
       await this.page.waitForTimeout(10000);
     }
+  }
+
+  async validateSuccessfulCreation() {
+    await this.validateNameSearchResult(this.randomFirstName);
   }
 
   async clickOnClearButton() {
@@ -154,6 +212,8 @@ export class CreatePatient {
     await this.enterFirstName();
     await this.enterLastName();
     await this.selectDateOfBirth();
+    await this.selectSexValue();
+    await this.selectDiseaseType();
   }
 
   async enterDataIntoOptionalFields() {
@@ -181,7 +241,7 @@ export class CreatePatient {
     expect(
       this.errorAlert.nth(4),
       "Disease required error message is incorrect"
-    ).toHaveText("Disease is required");
+    ).toHaveText("Disease type is required");
     expect(
       this.errorAlert.nth(5),
       "Affected Organ required error message is incorrect"
@@ -189,51 +249,69 @@ export class CreatePatient {
   }
 
   async validatePagination() {
-    await this.page.waitForTimeout(5000);
-    await this.totalItems.isVisible();
-    let totalItemsText = (await this.totalItems.innerText()).toString();
-    let totalItemsTextArray = totalItemsText.split(" ");
-    let totalItemsCount = totalItemsTextArray[1];
-    let recordsPerPageOption = ["20", "50", "100"];
-    let expectedPageCount = Math.ceil(Number(totalItemsCount) / 10);
-    let actualPageCount = (await this.pageNumberCount.innerText()).toString();
-    expect(expectedPageCount.toString(), "Page count is incorrect").toEqual(
-      actualPageCount
-    );
-    for (const recordsPerPageValue of recordsPerPageOption) {
-      await this.recordsPerPage.click();
-      await this.page
-        .locator("//*[@title='" + recordsPerPageValue + " / page']")
-        .click();
-      let expectedPageCount = Math.ceil(
-        Number(totalItemsCount) / Number(recordsPerPageValue)
-      );
+    if (await this.validatePatientRecordsAvailable()) {
+      await this.page.waitForTimeout(5000);
+      await this.totalItems.isVisible();
+      let totalItemsText = (await this.totalItems.innerText()).toString();
+      let totalItemsTextArray = totalItemsText.split(" ");
+      let totalItemsCount = totalItemsTextArray[1];
+      let recordsPerPageOption = ["20", "50", "100"];
+      let expectedPageCount = Math.ceil(Number(totalItemsCount) / 10);
       let actualPageCount = (await this.pageNumberCount.innerText()).toString();
       expect(expectedPageCount.toString(), "Page count is incorrect").toEqual(
         actualPageCount
       );
+      for (const recordsPerPageValue of recordsPerPageOption) {
+        await this.recordsPerPage.click();
+        await this.page
+          .locator("//*[@title='" + recordsPerPageValue + " / page']")
+          .click();
+        let expectedPageCount = Math.ceil(
+          Number(totalItemsCount) / Number(recordsPerPageValue)
+        );
+        let actualPageCount = (
+          await this.pageNumberCount.innerText()
+        ).toString();
+        expect(expectedPageCount.toString(), "Page count is incorrect").toEqual(
+          actualPageCount
+        );
+      }
+    } else {
+      console.log("No patient records available");
+    }
+  }
+
+  async validatePatientRecordsAvailable() {
+    await this.page.waitForTimeout(5000);
+    if (await this.createButton.isDisabled()) {
+      return true;
+    } else {
+      return false;
     }
   }
 
   async validateDefaultSort() {
-    let rank: string[] = [];
-    let flag = true;
-    let pageCount = await this.pageNumberCount.innerText();
-    for (let count = Number(pageCount); count > 0; count = count - 1) {
-      let tableData = await this.patientTable.innerText();
-      let allRecordsArray = tableData.split("\n");
-      allRecordsArray.splice(0, 12);
-      for (const element of allRecordsArray) {
-        let indexForMatchScore = element.lastIndexOf("\t");
-        let actualRankOrder = element.substring(indexForMatchScore + 1);
-        rank.push(actualRankOrder);
+    if (await this.validatePatientRecordsAvailable()) {
+      let rank: string[] = [];
+      let pageCount = await this.pageNumberCount.innerText();
+      for (let count = Number(pageCount); count > 0; count = count - 1) {
+        let tableData = await this.patientTable.innerText();
+        let allRecordsArray = tableData.split("\n");
+        allRecordsArray.splice(0, 12);
+        for (const element of allRecordsArray) {
+          let indexForMatchScore = element.lastIndexOf("\t");
+          let actualRankOrder = element.substring(indexForMatchScore + 1);
+          rank.push(actualRankOrder);
+        }
+        await this.nextPage.click();
+        await this.page.waitForTimeout(2000);
       }
-      await this.nextPage.click();
-      await this.page.waitForTimeout(2000);
+      let sortedRank = [...rank];
+      sortedRank.sort((one, two) => (one > two ? -1 : 1));
+      expect(rank, "Match score order is incorrect").toEqual(sortedRank);
+    } else {
+      console.log("No patient records available");
     }
-    let sortedRank = [...rank];
-    sortedRank.sort((one, two) => (one > two ? -1 : 1));
-    expect(rank, "Match score order is incorrect").toEqual(sortedRank);
   }
 
   async enterCharacterLimitDataInFirstName() {
@@ -270,5 +348,99 @@ export class CreatePatient {
       this.firstName,
       "Create patient form not visible"
     ).toBeVisible();
+  }
+
+  async validateClearButtonEnabled() {
+    await expect(this.clearButton, "Clear button is not enabled").toBeEnabled();
+  }
+
+  async validateFutureDateDisabled() {
+    await expect(
+      this.page.locator(
+        "//*[@class='ant-picker-cell-inner ant-picker-cell-today']"
+      ),
+      "Future date is enabled"
+    ).toBeDisabled();
+  }
+
+  async validateNameSearchResult(value: string) {
+    if (await this.validatePatientRecordsAvailable()) {
+      let pageCount = await this.pageNumberCount.innerText();
+      for (let count = Number(pageCount); count > 0; count = count - 1) {
+        let tableData = await this.patientTable.innerText();
+        let allRecordsArray = tableData.split("\n");
+        allRecordsArray.splice(0, 1);
+        for (const element of allRecordsArray) {
+          let removeMRN = element.substring(9);
+          let name = removeMRN.substring(0, removeMRN.indexOf("\t"));
+          expect(
+            name.toLowerCase(),
+            "Name search result is incorrect"
+          ).toContain(value);
+        }
+        await this.nextPage.click();
+        await this.page.waitForTimeout(2000);
+      }
+    } else {
+      console.log("No patient records available");
+    }
+  }
+
+  async validateDOBSearchResult() {
+    if (await this.validatePatientRecordsAvailable()) {
+      let pageCount = await this.pageNumberCount.innerText();
+      for (let count = Number(pageCount); count > 0; count = count - 1) {
+        let tableData = await this.patientTable.innerText();
+        let allRecordsArray = tableData.split("\n");
+        allRecordsArray.splice(0, 1);
+        for (const element of allRecordsArray) {
+          let removeRank = element.substring(0, element.lastIndexOf("\t"));
+          let DOB = removeRank.substring(removeRank.lastIndexOf("\t"));
+          const dobRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])\/20\d{2}$/;
+          expect(DOB.trim(), "DOB search result is incorrect").toEqual(
+            expect.stringMatching(dobRegex)
+          );
+        }
+        await this.nextPage.click();
+        await this.page.waitForTimeout(2000);
+      }
+    } else {
+      console.log("No patient records available");
+    }
+  }
+
+  async selectSexValue() {
+    let randomSexValue = this.sexDropdown[randomInt(0, 3)];
+    await this.sex.click();
+    await this.page.click("[title=" + randomSexValue + "]");
+  }
+
+  async selectDiseaseType() {
+    let randomDiseaseTypeValue = this.diseaseTypeDropdown[randomInt(0, 1)];
+    await this.page.getByText(randomDiseaseTypeValue).click();
+    await this.selectAffectedOrgan(randomDiseaseTypeValue);
+  }
+
+  async selectAffectedOrgan(randomDiseaseTypeValue: string) {
+    let randomAffectedOrganValue;
+    if (randomDiseaseTypeValue == "Cancer") {
+      randomAffectedOrganValue =
+        this.affectedOrganCancerDropdown[
+          randomInt(0, this.affectedOrganCancerDropdown.length - 1)
+        ];
+    } else if (randomDiseaseTypeValue == "Organ Failure") {
+      randomAffectedOrganValue =
+        this.affectedOrganOrganFailureDropdown[
+          randomInt(0, this.affectedOrganOrganFailureDropdown.length - 1)
+        ];
+    }
+    await this.affectedOrgan.click();
+    await this.affectedOrgan.fill(randomAffectedOrganValue);
+    await this.affectedOrgan.selectText(randomAffectedOrganValue);
+  }
+
+  async validateNoDataFound() {
+    await this.noDataAvailable.waitFor();
+    expect(await this.noDataAvailable.isVisible()).toBeTruthy();
   }
 }
