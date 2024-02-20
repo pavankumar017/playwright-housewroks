8bb56a2742d3ec4d795f0dde9fe785063638e614
pavankumar017 <pavankumarbr17@gmail.com>
2024-02-20 14:38:14 +0530

    6 sceanrios

--- hw-e2e/patient-master.spec.ts
+++ hw-e2e/patient-master.spec.ts
@@ -9,6 +9,7 @@ test("Test to verify patient Master page is displyed", async ({
   page,
   baseURL,
 }) => {
+  await page.goto(`${baseURL}`);
   const patientMaster = new PatientMaster(page);
   await patientMaster.heading.isVisible();
 });
@@ -17,14 +18,25 @@ test("Test to verify search patient in patient master ", async ({
   page,
   baseURL,
 }) => {
+  await page.goto(`${baseURL}`);
   const patientMaster = new PatientMaster(page);
   await patientMaster.heading.isVisible();
   await page.waitForTimeout(1000);
-  await patientMaster.search_enter();
+  await patientMaster.search_enter("james");
   await patientMaster.verify_search_result();
 });
 
+test("Test to verify search invalid patiend", async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.heading.isVisible();
+  await page.waitForTimeout(1000);
+  await patientMaster.search_enter("ASDASDASDasd");
+  await patientMaster.verify_no_data_found();
+});
+
 test("Test to validate criterias", async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}`);
   const patientMaster = new PatientMaster(page);
   await patientMaster.click_on_filters();
   await patientMaster.click_on_criteria_dropdown();
@@ -34,9 +46,27 @@ test("Test to validate criterias", async ({ page, baseURL }) => {
 test("Test to validate values of criteria Disease", async ({
   page,
   baseURL,
-}) => {});
+}) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.heading.isVisible();
+  await patientMaster.click_on_filters();
+  await patientMaster.click_on_criteria_dropdown();
+  await patientMaster.select_criteria_dropdown("Disease");
+  await patientMaster.click_on_select_dropdown();
+  await patientMaster.verify_disease_values();
+});
 
-test("Test to validate Organ Values", async ({ page, baseURL }) => {});
+test("Test to validate Organ Values", async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}`);
+  const patientMaster = new PatientMaster(page);
+  await patientMaster.heading.isVisible();
+  await patientMaster.click_on_filters();
+  await patientMaster.click_on_criteria_dropdown();
+  await patientMaster.select_criteria_dropdown("Organ");
+  await patientMaster.click_on_select_dropdown();
+  await patientMaster.verify_organ_values();
+});
 
 test("Test to validate values of criteria Phase", async ({
   page,
--- locators/patient-master.ts
+++ locators/patient-master.ts
@@ -7,9 +7,10 @@ export class PatientMaster {
   readonly heading: Locator;
   readonly search: Locator;
   readonly sear_result: Locator;
-  readonly search_key: string;
+  search_key_recieved: string;
   readonly filter: Locator;
   readonly criteria_dropdown: Locator;
+  readonly select_dropdown: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -20,10 +21,13 @@ export class PatientMaster {
     this.sear_result = this.page.locator(
       "//tbody[@class ='ant-table-tbody']/child::tr/td[2]"
     );
-    this.search_key = "James";
+    // this.search_key = "James";
     this.filter = this.page.locator("//*[@data-testid='down-arrow']");
     this.criteria_dropdown = this.page.locator(
       "//*[@data-testid='criteria-dropdown']"
+    );
+    this.select_dropdown = this.page.locator(
+      "//div[@data-testid='criteria-value-dropdown']"
     );
   }
 
@@ -31,14 +35,17 @@ export class PatientMaster {
     await this.createButton.click();
   }
 
-  async search_enter() {
-    await this.search.fill(this.search_key);
+  async search_enter(search_key) {
+    this.search_key_recieved = search_key;
+    await this.search.fill(this.search_key_recieved);
     await this.page.waitForTimeout(10000);
   }
 
   async verify_search_result() {
     let text = await this.sear_result.innerText();
-    await expect(text).toEqual(expect.stringContaining(this.search_key));
+    await expect(text).toEqual(
+      expect.stringContaining(this.search_key_recieved)
+    );
   }
 
   async click_on_filters() {
@@ -61,8 +68,63 @@ export class PatientMaster {
     for (let i = 1; i < 7; i++) {
       let current_text = await this.page
         .locator("//div[@title='DOB']/parent::div/div" + "[" + i + "]")
-        .allTextContents();
-      expect(current_text).toEqual(dropdown_expected[i]);
+        .textContent();
+      console.log(current_text);
+      console.log(dropdown_expected[i - 1]);
+      expect(current_text).toEqual(dropdown_expected[i - 1]);
+    }
+  }
+
+  async verify_no_data_found() {
+    await this.page.getByText("No patients found").isVisible();
+  }
+
+  async select_criteria_dropdown(criteria) {
+    await this.page.getByText(criteria, { exact: true }).click();
+  }
+
+  async click_on_select_dropdown() {
+    await this.select_dropdown.click();
+  }
+  async verify_disease_values() {
+    let disease_drop_down_values = ["Cancer", "Organ Failure"];
+
+    for (let i = 1; i <= disease_drop_down_values.length; i++) {
+      let current_values = await this.page
+        .locator("//div[@title = 'Cancer']/parent::div/div" + "[" + i + "]")
+        .textContent();
+      expect(current_values).toEqual(disease_drop_down_values[i - 1]);
+    }
+  }
+
+  async verify_organ_values() {
+    let organ_drop_down_values = [
+      "Kidney",
+      "Liver",
+      "Pancreas",
+      "Heart",
+      "Lung",
+      "Breast",
+      "Colon",
+      "Gallbladder",
+      "Stomach",
+      "Cervical",
+      "Ovarian",
+      "Uterine",
+      "Prostate",
+      "Thyroid",
+      "Melanoma",
+      "Biliary",
+      "Duodenal",
+    ];
+
+    for (let i = 1; i <= organ_drop_down_values.length; i++) {
+      let current_values = await this.page
+        .locator("//div[@title = 'Kidney']/parent::div/div" + "[" + i + "]")
+        .textContent();
+      expect(current_values).toEqual(organ_drop_down_values[i - 1]);
+      await this.page.keyboard.press("ArrowDown");
+      // await this.page.keyboardpress("Enter");
     }
   }
 }
